31b1dab1875c2825b371160e01ab73730afab57e
Yuxuan61305 <yuxuanliu050613@brandeis.edu>
2025-02-11 18:03:42 -0500

    Update print-addresses.c

--- print-addresses.c
+++ print-addresses.c
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]){
   for(int i = 1;i<argc;i++){
       long num = strtol(argv[i], NULL, 10);
-      printf("0X%012lX", num);
+      printf("0X%012lX\n", num);
     
   }
   
