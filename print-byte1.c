b49e214cc131522a340b7c725ec105ef061b214f
Yuxuan61305 <yuxuanliu050613@brandeis.edu>
2025-02-13 18:36:13 -0500

    Update print-byte1.c

--- print-byte1.c
+++ print-byte1.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
 
+
+# include<stdio.h>
+# include<stdlib.h>
 int main(int argc, char* argv[]){
     if(argc <2){
         return 0;
     }
     for(int i = 1;i<argc;i++){
-        long num = strtol(argv[i], NULL, 0);
+        long num = strtol(argv[i], NULL, 0) >> 8;
         printf("%d 0x%02lX %3ld\n",i, num & 0xFF, num&0xFF);
     }
     return 0;
