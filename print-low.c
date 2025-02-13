ebdb7a454eb68ee5618e265a2f5734d05b11314e
Yuxuan61305 <yuxuanliu050613@brandeis.edu>
2025-02-13 18:05:36 -0500

    Update print-low.c

--- print-low.c
+++ print-low.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <stdio.lib>
+#include <stdlib.h>
 
-
-int main(int argc, char *argv[]){
-  for(int i = 1; i <argc ;i++){
-    printf(i+" ");
-    long num = st
-    
-  }
-
+int main(int argc, char* argv[]){
+    if(argc <2){
+        return 0;
+    }
+    for(int i = 1;i<argc;i++){
+        long num = strtol(argv[i], NULL, 0);
+        printf("%d 0x%02lX %ld\n",i, num & 0xFF, num&0xFF);
+    }
+    return 0;
 }
